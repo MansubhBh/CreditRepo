de6d2c8e0eac815f17dfa208ed10ca889431e782
Boys <boys@Millys-MacBook-Air.local>
2019-02-16 15:53:05 +1100

    sql file add

--- sql/creditscore.sql
+++ sql/creditscore.sql
@@ -9,9 +9,9 @@ create table if not exists offer(
     `type` text,
     `limit` decimal(9,2),
      product_name text,
-     yearly_fee decimal(9,2),
      rewards text,
      balance_transfer decimal(9,2)
+     yearly_fee decimal(9,2),
 );
 
 
@@ -22,4 +22,5 @@ create table if not exists login(
 
 insert into login(username, password) values('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
 insert into login(username, password) values('user', '04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb');
+
 
